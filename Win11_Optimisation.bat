f71c7c01482ff46236c89ed08fcc7737118d49af
sbeteta42 <137415087+sbeteta42@users.noreply.github.com>
2024-02-22 13:38:50 +0100

    Add files via upload

--- Win11_Optimisation.bat
+++ Win11_Optimisation.bat
@@ -7,42 +7,42 @@ echo           Windows 11 Optimization Script By sbeteta@beteta.org
 echo =========================================================================
 
 :restorepoint
-set /p createRestorePoint=Créer d'abord un point de restauration ? (y/n)
+set /p createRestorePoint=Creer d'abord un point de restauration ? (y/n)
 if /i "%createRestorePoint%"=="y" (
     wmic /Namespace:\\root\default Path SystemRestore Call CreateRestorePoint "Optimization Script", 100, 7
-    echo Point de restauration créé.
+    echo Point de restauration cree.
 ) else if /i "%createRestorePoint%"=="n" (
-    echo Echec pour créer le point de restauration.
+    echo Echec pour creer le point de restauration.
 ) else (
-    echo Entrée invalide. Entrez s'il vous plait y ou n.
+    echo Entree invalide. Entrez s'il vous plait y ou n.
     goto restorepoint
 )
 
 :disableipv6
-set /p disableIPV6=Désactivé IPv6? (y/n)
+set /p disableIPV6=Desactive IPv6? (y/n)
 if /i "%disableIPV6%"=="y" (
-    netsh interface ipv6 set interface "Ethernet" Désactivation
-    netsh interface ipv6 set interface "Wi-Fi" Désactivation
-    echo IPv6 désactivé.
+    netsh interface ipv6 set interface "Ethernet" Desactivation
+    netsh interface ipv6 set interface "Wi-Fi" Desactivation
+    echo IPv6 desactive.
 ) else if /i "%disableIPV6%"=="n" (
-    echo Echec Désactivation de IPv6.
+    echo Echec Desactivation de IPv6.
 ) else (
-    echo Entrée invalide. Entrez s'il vous plait y ou n.
+    echo Entree invalide. Entrez s'il vous plait y ou n.
     goto disableipv6
 )
 
 :optimizenetwork
-set /p optimizeNetwork=Optimiser les paramètres réseaux ? (y/n)
+set /p optimizeNetwork=Optimiser les paramètres reseaux ? (y/n)
 if /i "%optimizeNetwork%"=="y" (
     netsh int tcp set global autotuninglevel=normal
     netsh int tcp set global rss=enabled
     reg add "HKLM\Software\Microsoft\PolicyManager\default\WiFi\AllowWiFiHotSpotReporting" /v "Value" /t REG_DWORD /d 0 /f
     reg add "HKLM\Software\Microsoft\PolicyManager\default\WiFi\AllowAutoConnectToWiFiSenseHotspots" /v "Value" /t REG_DWORD /d 0 /f
-    echo Paramètres réseau optimisés.
+    echo Paramètres reseau optimises.
 ) else if /i "%optimizeNetwork%"=="n" (
-    echo Echec pour optimiser les paramètres réseau
+    echo Echec pour optimiser les paramètres reseau
 ) else (
-    echo Entrée invalide. Entrez s'il vous plait y ou n.
+    echo Entree invalide. Entrez s'il vous plait y ou n.
     goto optimizenetwork
 )
 
@@ -52,81 +52,81 @@ if /i "%clearTempFiles%"=="y" (
     del /s /q /f %temp%\*
     rd /s /q %temp%
     md %temp%
-    echo Fichiers temporaires effacés.
+    echo Fichiers temporaires effaces.
 ) else if /i "%clearTempFiles%"=="n" (
-    echo Echec sur la procédure "Fichiers temporaires effacés."
+    echo Echec sur la procedure "Fichiers temporaires effaces."
 ) else (
-    echo Entrée invalide. Entrez s'il vous plait y ou n.
+    echo Entree invalide. Entrez s'il vous plait y ou n.
     goto cleartempfiles
 )
 
 :defraghdd
-set /p defragHDD=Défragmenter un disque dur? (y/n)
+set /p defragHDD=Defragmenter un disque dur? (y/n)
 if /i "%defragHDD%"=="y" (
     defrag C: /U /V
-    echo Disque dur défragmenté.
+    echo Disque dur defragmente.
 ) else if /i "%defragHDD%"=="n" (
-    echo Echec sur la défragmentation du disque.
+    echo Echec sur la defragmentation du disque.
 ) else (
-    echo Entrée invalide. Entrez s'il vous plait y ou n.
+    echo Entree invalide. Entrez s'il vous plait y ou n.
     goto defraghdd
 )
 
 :disablegamemode
-set /p disableGameMode=Désactiver le mode jeu ? (y/n)
+set /p disableGameMode=Desactiver le mode jeu ? (y/n)
 if /i "%disableGameMode%"=="y" (
     reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\GameDVR" /v "AllowGameDVR" /t REG_DWORD /d 0 /f
-    echo Mode jeu désactivé.
+    echo Mode jeu desactive.
 ) else if /i "%disableGameMode%"=="n" (
-    echo Echec sur la procédure "Mode jeu désactivé."
+    echo Echec sur la procedure "Mode jeu desactive."
 ) else (
-    echo Entrée invalide. Entrez s'il vous plait y ou n.
+    echo Entree invalide. Entrez s'il vous plait y ou n.
     goto disablegamemode
 )
 
 :disablegamebar
-set /p disableGameBar=Désactiver la barre de jeu ? (y/n)
+set /p disableGameBar=Desactiver la barre de jeu ? (y/n)
 if /i "%disableGameBar%"=="y" (
     PowerShell -Command "Get-AppxPackage *Microsoft.XboxGamingOverlay* | Remove-AppxPackage"
     reg add "HKCU\Software\Microsoft\GameBar" /v "AllowAutoGameMode" /t REG_DWORD /d 0 /f
-    echo Game Bar Désactivation.
+    echo Game Bar Desactivation.
 ) else if /i "%disableGameBar%"=="n" (
-    echo Echec sur la procédure "Désactiver la barre de jeu".
+    echo Echec sur la procedure "Desactiver la barre de jeu".
 ) else (
-    echo Entrée invalide. Entrez s'il vous plait y ou n.
+    echo Entree invalide. Entrez s'il vous plait y ou n.
     goto disablegamebar
 )
 
 :disableindexing
-set /p disableIndexing=Désactiver l'indexation sur tous les lecteurs ? (y/n)
+set /p disableIndexing=Desactiver l'indexation sur tous les lecteurs ? (y/n)
 if /i "%disableIndexing%"=="y" (
     reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\Windows Search" /v "AllowIndexingEncryptedStoresOrItems" /t REG_DWORD /d 0 /f
-    echo Indexing Désactivation on all drives.
+    echo Indexing Desactivation on all drives.
 ) else if /i "%disableIndexing%"=="n" (
-    echo Echec de la procédure "Désactiver l'indexation sur tous les lecteurs".
+    echo Echec de la procedure "Desactiver l'indexation sur tous les lecteurs".
 ) else (
-    echo Entrée invalide. Entrez s'il vous plait y ou n.
+    echo Entree invalide. Entrez s'il vous plait y ou n.
     goto disableindexing
 )
 
 :disablehibernation
-set /p disableHibernation=Désactiver l'hibernation ? (y/n)
+set /p disableHibernation=Desactiver l'hibernation ? (y/n)
 if /i "%disableHibernation%"=="y" (
     powercfg -h off
  reg add "HKLM\System\CurrentControlSet\Control\Session Manager\Power" /v HibernateEnabled /t REG_DWORD /d 0 /f
     reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\FlyoutMenuSettings" /v ShowHibernateOption /t REG_DWORD /d 0 /f
     reg add "HKLM\SYSTEM\CurrentControlSet\Control\Power\PowerThrottling" /v PowerThrottlingOff /t REG_DWORD /d 00000001 /f
     reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Power" /v HiberbootEnabled /t REG_DWORD /d 00000000 /f
-    echo Hibernation Désactivation.
+    echo Hibernation Desactivation.
 ) else if /i "%disableHibernation%"=="n" (
-    echo Echec de la procédure "Désactiver l'hibernation".
+    echo Echec de la procedure "Desactiver l'hibernation".
 ) else (
-    echo Entrée invalide. Entrez s'il vous plait y ou n.
+    echo Entree invalide. Entrez s'il vous plait y ou n.
     goto disablehibernation
 )
 
 :disabletelemetry
-set /p disableTelemetry=Désactiver la télémétrie et la collecte de données ? (y/n)
+set /p disableTelemetry=Desactiver la telemetrie et la collecte de donnees ? (y/n)
 if /i "%disableTelemetry%"=="y" (
 
     schtasks /Change /TN "Microsoft\Windows\Application Experience\Microsoft Compatibility Appraiser" /DISABLE
@@ -158,8 +158,8 @@ reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\CloudContent" /v "DisableWindo
 reg add "HKCU\SOFTWARE\Microsoft\Siuf\Rules" /v "NumberOfSIUFInPeriod" /t REG_DWORD /d 0 /f
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\DataCollection" /v "DoNotShowFeedbackNotifications" /t REG_DWORD /d 1 /f
 reg add "HKCU\SOFTWARE\Policies\Microsoft\Windows\CloudContent" /v "DisableTailoredExperiencesWithDiagnosticData" /t REG_DWORD /d 1 /f
-reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\AdvertisingInfo" /v "DésactivationByGroupPolicy" /t REG_DWORD /d 1 /f
-reg add "HKLM\SOFTWARE\Microsoft\Windows\Windows Error Reporting" /v "Désactivation" /t REG_DWORD /d 1 /f
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\AdvertisingInfo" /v "DesactivationByGroupPolicy" /t REG_DWORD /d 1 /f
+reg add "HKLM\SOFTWARE\Microsoft\Windows\Windows Error Reporting" /v "Desactivation" /t REG_DWORD /d 1 /f
 reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\DeliveryOptimization\Config" /v "DODownloadMode" /t REG_DWORD /d 1 /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\Remote Assistance" /v "fAllowToGetHelp" /t REG_DWORD /d 0 /f
 reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\OperationStatusManager" /v "EnthusiastMode" /t REG_DWORD /d 1 /f
@@ -185,16 +185,16 @@ reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Multimedia\SystemProf
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\DiagTrack" /v "Start" /t REG_DWORD /d 4 /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\dmwappushservice" /v "Start" /t REG_DWORD /d 4 /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\SysMain" /v "Start" /t REG_DWORD /d 4 /f
-    echo Télémétrie et collecte de données désactivé.
+    echo Telemetrie et collecte de donnees desactive.
 ) else if /i "%disableTelemetry%"=="n" (
-    echo  echec de la procédure "Désactiver la télémétrie et la collecte de données".
+    echo  echec de la procedure "Desactiver la telemetrie et la collecte de donnees".
 ) else (
-    echo Entrée invalide. Entrez s'il vous plait y ou n.
+    echo Entree invalide. Entrez s'il vous plait y ou n.
     goto disabletelemetry
 )
 
 :disabletips
-set /p disableTips=Désactiver les conseils/suggestions Windows ? (y/n)
+set /p disableTips=Desactiver les conseils/suggestions Windows ? (y/n)
 if /i "%disableTips%"=="y" (
     reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" /v "SubscribedContent-310093Enabled" /t REG_DWORD /d 0 /f
     reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" /v "SubscribedContent-338387Enabled" /t REG_DWORD /d 0 /f
@@ -202,102 +202,102 @@ if /i "%disableTips%"=="y" (
     reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" /v "SubscribedContent-338389Enabled" /t REG_DWORD /d 0 /f
     reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" /v "SubscribedContent-353694Enabled" /t REG_DWORD /d 0 /f
     reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" /v "SubscribedContent-353696Enabled" /t REG_DWORD /d 0 /f
-    echo Windows tips/suggestions Désactivation.
+    echo Windows tips/suggestions Desactivation.
 ) else if /i "%disableTips%"=="n" (
-    echo Echec de la procédure "Désactiver les conseils/suggestions Windows".
+    echo Echec de la procedure "Desactiver les conseils/suggestions Windows".
 ) else (
-    echo Entrée invalide. Entrez s'il vous plait y ou n.
+    echo Entree invalide. Entrez s'il vous plait y ou n.
     goto disabletips
 )
 
 :disablehomegroup
-set /p disableHomeGroup=Désactiver le groupe résidentiel ? (y/n)
+set /p disableHomeGroup=Desactiver le groupe residentiel ? (y/n)
 if /i "%disableHomeGroup%"=="y" (
     reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v "EnableHomeGroup" /t REG_DWORD /d 0 /f
-    echo HomeGroup désactivé.
+    echo HomeGroup desactive.
 ) else if /i "%disableHomeGroup%"=="n" (
-    echo Echec de la procédure "Désactiver le groupe résidentiel"
+    echo Echec de la procedure "Desactiver le groupe residentiel"
 ) else (
-    echo Entrée invalide. Entrez s'il vous plait y ou n.
+    echo Entree invalide. Entrez s'il vous plait y ou n.
     goto disablehomegroup
 )
 
 :disablestickykeys
-set /p disableStickyKeys=Désactiver l'invite des touches rémanentes ? (y/n)
+set /p disableStickyKeys=Desactiver l'invite des touches remanentes ? (y/n)
 if /i "%disableStickyKeys%"=="y" (
     reg add "HKCU\Control Panel\Accessibility\StickyKeys" /v "Flags" /t REG_SZ /d "506" /f
-    echo Sticky keys prompt Désactivation.
+    echo Sticky keys prompt Desactivation.
 ) else if /i "%disableStickyKeys%"=="n" (
-    echo Echec de la procédure "Désactiver l'invite des touches rémanentes".
+    echo Echec de la procedure "Desactiver l'invite des touches remanentes".
 ) else (
-    echo Entrée invalide. Entrez s'il vous plait y ou n.
+    echo Entree invalide. Entrez s'il vous plait y ou n.
     goto disablestickykeys
 )
 
 :disablesuperfetch
-set /p disableSuperfetch=Désactiver Superfetch ? (y/n)
+set /p disableSuperfetch=Desactiver Superfetch ? (y/n)
 if /i "%disableSuperfetch%"=="y" (
-    sc config SysMain start=Désactivation
+    sc config SysMain start=Desactivation
     reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management\PrefetchParameters" /v EnableSuperfetch /t REG_DWORD /d 0 /f
     reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management\PrefetchParameters" /v EnablePrefetcher /t REG_DWORD /d 0 /f
-    echo Superfetch Désactivation.
+    echo Superfetch Desactivation.
 ) else if /i "%disableSuperfetch%"=="n" (
-    echo Echec de la procédure "Désactiver Superfetch".
+    echo Echec de la procedure "Desactiver Superfetch".
 ) else (
-    echo Entrée invalide. Entrez s'il vous plait y ou n.
+    echo Entree invalide. Entrez s'il vous plait y ou n.
     goto disablesuperfetch
 )
 
 :spooler
-set /p disablespooler=Désactiver le spouleur d'impression (y/n)
+set /p disablespooler=Desactiver le spouleur d'impression (y/n)
 if /i "%disablespooler%"=="y" (
     PowerShell -Command net stop spooler
-    echo Spooler Désactivation.
+    echo Spooler Desactivation.
 ) else if /i "%disablespooler%"=="n" (
-    echo Echec de la procédure "Désactiver le spouleur".
+    echo Echec de la procedure "Desactiver le spouleur".
 ) else (
-    echo Entrée invalide. Entrez s'il vous plait y ou n.
+    echo Entree invalide. Entrez s'il vous plait y ou n.
     goto spooler
 )
 
 :WindowsInsiderService
-set /p disableWindowsInsiderService= Désactiver le service Windows Insider (y/n)
+set /p disableWindowsInsiderService= Desactiver le service Windows Insider (y/n)
 if /i "%disableWindowsInsiderService%"=="y" (
-    PowerShell -Command Set-Service wisvc -StartupType Désactivation
-    echo Désactivation Windows Insider Service.
+    PowerShell -Command Set-Service wisvc -StartupType Desactivation
+    echo Desactivation Windows Insider Service.
 ) else if /i "%disableWindowsInsiderService%"=="n" (
-    echo Echec de la procédure "Désactiver le service Windows Insider"
+    echo Echec de la procedure "Desactiver le service Windows Insider"
 ) else (
-    echo Entrée invalide. Entrez s'il vous plait y ou n.
+    echo Entree invalide. Entrez s'il vous plait y ou n.
     goto WindowsInsiderService
 )
 
 :DiagnosticServiceHost
-set /p DésactivationiagnosticServiceHost= Désactiver l'hôte du service de diagnostic (y/n)
-if /i "%DésactivationiagnosticServiceHost%"=="y" (
+set /p DesactivationiagnosticServiceHost= Desactiver l'hôte du service de diagnostic (y/n)
+if /i "%DesactivationiagnosticServiceHost%"=="y" (
     PowerShell -Command "net stop DPS"
-    echo Echec de la procédure "Désactiver l'hôte du service de diagnostic"
-) else if /i "%DésactivationiagnosticServiceHost%"=="n" (
-    echo Echec de la procédure"Désactiver l'hôte du service de diagnostic".
+    echo Echec de la procedure "Desactiver l'hôte du service de diagnostic"
+) else if /i "%DesactivationiagnosticServiceHost%"=="n" (
+    echo Echec de la procedure"Desactiver l'hôte du service de diagnostic".
 ) else (
-    echo Entrée invalide. Entrez s'il vous plait y ou n.
+    echo Entree invalide. Entrez s'il vous plait y ou n.
     goto DiagnosticServiceHost
 )
 
 :BITS
-set /p disableBITS= Désactiver BITS ()Background Intelligent Transfer Service)(y/n)
+set /p disableBITS= Desactiver BITS ()Background Intelligent Transfer Service)(y/n)
 if /i "%disableBITS%"=="y" (
     net stop wuauserv
     net stop cryptSvc
     net stop bits
     net stop msiserver
-    echo Désactivation BITS.
+    echo Desactivation BITS.
 ) else if /i "%disableBITS%"=="n" (
-    echo Echec de la procédure "Désactiver BITS".
+    echo Echec de la procedure "Desactiver BITS".
 ) else (
-    echo Entrée invalide. Entrez s'il vous plait y ou n.
+    echo Entree invalide. Entrez s'il vous plait y ou n.
     goto BITS
 
 
-echo Optimisation terminée ! Merci
+echo Optimisation terminee ! Merci
 pause 
